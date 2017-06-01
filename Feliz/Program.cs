b597bd7ec8029a497fae62536c3309bd4ecd02c9
aspnetwfgmailcom <wtecnologia@outlook.com.br>
2017-06-01 17:45:03 -0300

    Teste
    Exercício 01/06/2017

---
+++ Feliz/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            List<int> lista = new List<int>();
+            for (int i = 0; i < 100; i++)
+                if (VerificarNumero(i)) lista.Add(i);
+            var recebe = GerarListaNumeros();
+
+            lista.ForEach(i => Console.WriteLine("Feliz {0}", i));
+            Console.ReadKey();
+
+        }        
+
+        public static List<int> GerarListaNumeros()
+        {
+            List<int> lista = new List<int>();
+            for (int i = 0; i < 100; i++)
+                if (VerificarNumero(i)) lista.Add(i);
+            return lista;
+        }
+
+        public static bool VerificarNumero(int numero)
+        {
+            bool numeroFeliz = false;
+            List<int> listaDigitos = new List<int>();
+            listaDigitos = DividirDigitos(numero);
+            for (int i = 0; i < 20 && !numeroFeliz; i++)
+            {
+                int sumaActual = CalcularQuadrados(listaDigitos);
+                if (sumaActual != 1)
+                    listaDigitos = DividirDigitos(sumaActual);
+                else numeroFeliz = true;
+            }
+            return numeroFeliz;
+        }
+        
+        public static List<int> DividirDigitos(int digito)
+        {
+            List<int> digitos = new List<int>();
+            while (digito != 0)
+            {
+                digitos.Add(digito % 10);
+                digito = digito / 10;
+            }
+            return digitos;
+        }
+        
+        public static int CalcularQuadrados(List<int> listaDigitos)
+        {
+            int resultado = 0;
+            foreach (int elem in listaDigitos) resultado += elem * elem;
+            return resultado;
+        }
+    }
+}
---
+++ Sortudo/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public class NumeroSortudo
+        {
+            public string GetNumeroSortudo(int range)
+            {
+                return ImprimirResultado(FindNumeroSortudo(range));
+            }
+
+            public string GetsortudoPrimoNumeros(int range)
+            {
+                bool[] numeros = FindNumeroSortudo(range);
+
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    if (numeros[i]) continue;
+                    numeros[i] = !NumeroPrimo(i + 1);
+                }
+
+                return ImprimirResultado(numeros);
+            }
+
+            private bool[] FindNumeroSortudo(int range)
+            {
+                if (range < 1) range = 0;
+                bool[] numeros = new bool[range];
+                int sortudoContar = 2;
+
+                while (sortudoContar < numeros.Length)
+                {
+                    sortudoContar = NumeroFora(numeros, sortudoContar);
+                }
+
+                return numeros;
+            }
+
+            private int NumeroFora(bool[] numeros, int sortudoContar)
+            {
+                int contador = 0;
+
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    if (numeros[i]) continue;
+                    contador++;
+
+                    if (contador == sortudoContar)
+                    {
+                        numeros[i] = true;
+                        contador = 0;
+                    }
+                }
+
+                return GetsortudoContar(numeros, sortudoContar);
+            }
+
+            private int GetsortudoContar(bool[] numeros, int pular)
+            {
+                if (pular >= numeros.Length) return numeros.Length;
+
+                for (int i = pular; i < numeros.Length; i++)
+                {
+                    if (!numeros[i]) return i + 1;
+                }
+
+                return numeros.Length;
+            }
+
+            private static bool NumeroPrimo(int numero)
+            {
+                if (numero == 1) return false;
+
+                for (short i = 3; i <= Math.Sqrt(numero); i += 2)
+                {
+                    if (numero % i == 0) return false;
+                }
+
+                return true;
+            }
+
+            private static string ImprimirResultado(bool[] numeros)
+            {
+                string resultado = string.Empty;
+
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    if (!numeros[i]) resultado = resultado + (i + 1) + ",";
+                }
+
+                if (!string.IsNullOrEmpty(resultado)) resultado = resultado.Substring(0, resultado.Length - 1);
+
+                return resultado;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // arrange
+            NumeroSortudo sortudo = new NumeroSortudo();
+            IDictionary<int, string> CasoTeste = new Dictionary<int, string>();
+            for (int i = 0; i < 100; i++)
+            {
+                CasoTeste.Add(i, Convert.ToString(i));                
+            }
+            // act
+            foreach (var testCase in CasoTeste)
+            {
+                string resultado = sortudo.GetNumeroSortudo(testCase.Key);
+
+                // assert
+                Console.WriteLine("{0}", resultado);
+                
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
